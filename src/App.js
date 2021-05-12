0606b5b4b6d356348387aeddcec92e64cfaf45f2
cooligc <sitakant.chaudhury@gmail.com>
2021-05-12 17:56:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
+import Button from './theme/Button';
+import theme from './theme/theme';
 
 function App() {
+
+
+  const [isDark, setDark] = useState(true)
+  const [style, setStyle] = useState(theme.dark)
+  const hi = {"height":"100px", "width" : "100px"}
+  
+  const onClick = () => {
+    console.log("Selected " + isDark)
+    const sty = isDark ? theme.dark : theme.light;
+    setDark(!isDark)
+    setStyle(sty)
+    console.log("Selected 2" + isDark)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+         Dark Mode : <input type="radio" name="theme" onChange={onClick}/>
+         Light Mode : <input type="radio" name="theme" onChange={onClick}/>
       </header>
+        <Button name="Click Me" style={style}/>
     </div>
   );
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+    "dark" : {
+        "background" : "#7054e0",
+        "color" : "#54c0e0",
+    },
+    "light" : {
+        "background" : "#a393e4",
+        "color" : "#8bc8da"
+    }
+}
+
+export default theme;
